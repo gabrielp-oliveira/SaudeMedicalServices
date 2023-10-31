3735fbd956c9c8e55f679b14d8b44f9945287b4e
gabriel-p-oliveira <goliveir@cswg.com>
2023-10-31 00:33:01 -0400

    fix responsive

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
         <span>Copyright (c) 2023</span>
         <span><a href="mailTo: saudemedicalservices@gmail.com">saudemedicalservices@gmail.com</a></span>
       </div>
-      <div>
+      <div> 
         <img src={lock} alt='lock icon image'></img>
         <span>Secure 256-bit SSL encryption.</span>
       </div>
